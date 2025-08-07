4ba1e0f83242b255d79e6219c3180a3c1a1b8b7d
William4954t <williamfrancootero@gmail.com>
2025-08-06 22:33:50 -0500

    inicio de interfaz en terminal

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from audit import aplicaciones
 
 if __name__ == "__main__":
-    aplicaciones.verificar_aplicaciones()# <-- Llenar para filtrar
-    print("FIN DE LA AUDITORIA.")
-    input("\nPresiona ENTER para salir...")
+
+    while True:
+    
+        print("---Inicio de la auditoria---")
+        print("1. Verificar aplicaciones")
+        print("2. Salir")
+        opcion = input("Selecciona una opcion: ")
+
+        match opcion:
+            case "1":
+                aplicaciones.verificar_aplicaciones("adobe")
+                aplicaciones.verificar_aplicaciones("java")
+                aplicaciones.verificar_aplicaciones("365")
+            case "2":
+                print("borrando temporales...")
+            case "3":
+                print("Saliendo de la auditoria...")
+                break
+            case _:
+                print("Opcion no valida. Intente de nuevo.")
